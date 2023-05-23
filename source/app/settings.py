60a15035fd5f1c853830e8f9de3f2efa0856a1b7
Abylay Kuvatov <abukuvatov@icloud.com>
2023-05-23 22:42:33 +0600

    #35 Установил библиотеку django-bootstrap-v5, а также обновил файл requirements.txt

--- source/app/settings.py
+++ source/app/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'smarttender'
+    'smarttender',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
