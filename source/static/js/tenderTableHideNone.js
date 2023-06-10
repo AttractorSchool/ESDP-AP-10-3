282cf9354867041cebebdd876b1b539be16e76f9
Abylay Kuvatov <abukuvatov@icloud.com>
2023-06-11 02:05:16 +0600

    #65 Добавил JS скрипт для скрытия значения 'None' в таблице объявлений (тендеров)

---
+++ source/static/js/tenderTableHideNone.js
@@ -0,0 +1,11 @@
+const table = document.getElementById('tenderTable');
+
+if(table){
+const cells = table.getElementsByTagName('td');
+for(let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    if(cell.textContent.trim() === 'None') {
+        cell.textContent = ' ';
+    }
+}
+}
