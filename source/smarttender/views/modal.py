82174149eca9fb10de0d59fc105e6856b49c3b2e
Abylay Kuvatov <abukuvatov@icloud.com>
2023-06-11 14:52:34 +0600

    #65 Изменил представление получения подробной информации о тендере, а также вынес модальное окно подробной информации о тендере в частичный шаблон

--- source/smarttender/views/modal.py
+++ source/smarttender/views/modal.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
 from smarttender.models import Tender
 
 
@@ -7,26 +8,26 @@ def get_cell_data(request):
     cell_id = request.GET.get('cell_id')
     tender = get_object_or_404(Tender, id=cell_id)
     data = {
-        'lot': tender.lot,
-        'company': tender.company,
-        'name': tender.name,
-        'price_per_unit': tender.price_per_unit,
-        'quantity': tender.quantity,
-        'planned_amount': tender.planned_amount,
-        'delivery_deadline': tender.delivery_deadline,
-        'proposed_product_name': tender.proposed_product_name,
-        'supplier': tender.supplier,
-        'price_without_discount': tender.price_without_discount,
+        'lot_number': tender.lot.lot_number if tender.lot else None,
+        'customer_name_ru': tender.lot.customer_name_ru if tender.lot else None,
+        'name_ru': tender.lot.name_ru if tender.lot else None,
+        'description_ru': tender.lot.description_ru if tender.lot else None,
+        'price': tender.lot.plans.price if tender.lot and tender.lot.plans else None,
+        'count': tender.lot.plans.count if tender.lot and tender.lot.plans else None,
+        'ref_unit': tender.lot.plans.ref_units.name_ru if tender.lot and tender.lot.plans and tender.lot.plans.ref_units else None,
+        'amount': tender.lot.plans.amount if tender.lot and tender.lot.plans else None,
+        'supply_date_ru': tender.lot.plans.supply_date_ru if tender.lot and tender.lot.plans else None,
+        'products': tender.lot.products.trade_name if tender.lot and tender.lot.products else None,
+        'suppliers': tender.lot.suppliers.name if tender.lot and tender.lot.suppliers else None,
         'supplier_discount': tender.supplier_discount,
-        'price_with_discount': tender.price_with_discount,
         'vat': tender.vat,
         'note': tender.note,
         'manager': tender.manager,
         'purchase_price': tender.purchase_price,
         'overall_info': tender.overall_info,
-        'date': tender.date,
-        'deadline': tender.deadline,
-        'procurement_type': tender.procurement_type,
+        'publish_date': tender.lot.trd_buy.publish_date if tender.lot and tender.lot.trd_buy else None,
+        'end_date': tender.lot.trd_buy.end_date if tender.lot and tender.lot.trd_buy else None,
+        'ref_trade_method': tender.lot.trd_buy.ref_trade_methods.name_ru if tender.lot and tender.lot.trd_buy and tender.lot.trd_buy.ref_trade_methods else None,
         'paper_ad_link': tender.paper_ad_link,
         'lot_link': tender.lot_link,
         'profit_rate': tender.profit_rate,
@@ -39,18 +40,6 @@ def get_cell_data(request):
         'overall_contract_amount': tender.overall_contract_amount,
         'winning_price': tender.winning_price,
         'commercial_offer_text': tender.commercial_offer_text,
-        'status': tender.status,
+        'status': tender.status
     }
     return JsonResponse(data)
-
-
-
-
-
-
-
-
-
-
-
-
--- source/static/js/modal.js
+++ source/static/js/modal.js
@@ -9,25 +9,28 @@
                 'cell_id': cellId
             },
             success: function (data) {
-                $('#modal-lot').text(data.lot);
-                $('#modal-company').text(data.company);
-                $('#modal-name').text(data.name);
-                $('#modal-price_per_unit').text(data.price_per_unit);
-                $('#modal-quantity').text(data.quantity);
-                $('#modal-planned_amount').text(data.planned_amount);
-                $('#modal-delivery_deadline').text(data.delivery_deadline);
-                $('#modal-proposed_product_name').text(data.proposed_product_name);
-                $('#modal-supplier').text(data.supplier);
-                $('#modal-price_without_discount').text(data.price_without_discount);
+                $('#modal-lot_number').text(data.lot_number);
+                $('#modal-customer_name_ru').text(data.customer_name_ru);
+                $('#modal-name_ru').text(data.name_ru);
+                $('#modal-description_ru').text(data.description_ru);
+                $('#modal-price').text(data.price);
+                $('#modal-count').text(data.count);
+                $('#modal-ref_unit').text(data.ref_unit);
+                $('#modal-amount').text(data.amount);
+                $('#modal-supply_date_ru').text(data.supply_date_ru);
+                $('#modal-products').text(data.products);
+                $('#modal-suppliers').text(data.suppliers);
+//                $('#modal-price_without_discount').text(data.price_without_discount);
                 $('#modal-supplier_discount').text(data.supplier_discount);
-                $('#modal-price_with_discount').text(data.price_with_discount);
+//                $('#modal-price_with_discount').text(data.price_with_discount);
                 $('#modal-vat').text(data.vat);
                 $('#modal-note').text(data.note);
                 $('#modal-manager').text(data.manager);
                 $('#modal-purchase_price').text(data.purchase_price);
-                $('#modal-date').text(data.date);
-                $('#modal-deadline').text(data.deadline);
-                $('#modal-procurement_type').text(data.procurement_type);
+                $('#modal-overall_info').text(data.overall_info);
+                $('#modal-publish_date').text(data.publish_date);
+                $('#modal-end_date').text(data.end_date);
+                $('#modal-ref_trade_method').text(data.ref_trade_method);
                 $('#modal-paper_ad_link').text(data.paper_ad_link);
                 $('#modal-lot_link').text(data.lot_link);
                 $('#modal-profit_rate').text(data.profit_rate);
