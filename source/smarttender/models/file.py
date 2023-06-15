a9b7f7bce5af59044e3724bfc072b9225d597387
Abylay Kuvatov <abukuvatov@icloud.com>
2023-06-15 13:58:37 +0600

    #79 Переопределил модель Файл

--- source/smarttender/models/file.py
+++ source/smarttender/models/file.py
@@ -2,11 +2,34 @@ from django.db import models
 
 
 class File(models.Model):
+    original_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Оригинальное имя файла'
+    )
+    name_kz = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование документа на государственном языке'
+    )
+    name_ru = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование документа на русском языке'
+    )
     file_path = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        verbose_name="Документ лота"
+        verbose_name="Путь до файла"
+    )
+    trd_buy = models.ForeignKey(
+        to='smarttender.TrdBuy',
+        on_delete=models.CASCADE,
+        related_name='files'
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
--- source/smarttender/models/trd_buy.py
+++ source/smarttender/models/trd_buy.py
@@ -2,6 +2,23 @@ from django.db import models
 
 
 class TrdBuy(models.Model):
+    ref_trade_methods = models.ForeignKey(
+        to="smarttender.RefTradeMethod",
+        on_delete=models.CASCADE
+    )
+
+    numberAnno = models.CharField(max_length=255, null=True, blank=True)
+    nameKz = models.CharField(max_length=255, null=True, blank=True)
+    nameRu = models.CharField(max_length=255, null=True, blank=True)
+    totalSum = models.CharField(max_length=100, null=True, blank=True)
+    countLots = models.IntegerField(null=True, blank=True)
+    customerNameKz = models.TextField(null=True, blank=True)
+    customerNameRu = models.TextField(null=True, blank=True)
+    orgBin = models.CharField(max_length=255, null=True, blank=True)
+    orgPid = models.CharField(max_length=255, null=True, blank=True)
+    orgNameKz = models.CharField(max_length=255, null=True, blank=True)
+    orgNameRu = models.CharField(max_length=255, null=True, blank=True)
+    startDate = models.DateField(null=True, blank=True)
     publish_date = models.CharField(
         max_length=100,
         null=True,
@@ -14,10 +31,9 @@ class TrdBuy(models.Model):
         blank=True,
         verbose_name="Крайний срок"
     )
-    ref_trade_methods = models.ForeignKey(
-        to="smarttender.RefTradeMethod",
-        on_delete=models.CASCADE
-    )
+    itogiDatePublic = models.DateField(null=True, blank=True)
+    finYear = models.IntegerField(null=True, blank=True)
+
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата и время создания"
