67b7eaa2fc65b725bd35dea84b47c6c7a510f575
Abylay Kuvatov <abukuvatov@icloud.com>
2023-06-15 14:54:02 +0600

    #79 Переопределил модель Единица измерения

--- source/smarttender/models/ref_unit.py
+++ source/smarttender/models/ref_unit.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 
 class RefUnit(models.Model):
+    name_kz = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name="Единица измерения на государственном языке"
+    )
     name_ru = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        verbose_name="Единица измерения"
+        verbose_name="Единица измерения на русском языке"
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
@@ -16,3 +23,21 @@ class RefUnit(models.Model):
         auto_now=True,
         verbose_name="Дата и время изменения"
     )
+    is_deleted = models.BooleanField(
+        null=False,
+        default=False,
+        verbose_name='Удалён'
+    )
+    deleted_at = models.DateTimeField(
+        null=True,
+        default=None,
+        verbose_name='Дата и время удаления'
+    )
+
+    def __str__(self):
+        return self.name_ru
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
