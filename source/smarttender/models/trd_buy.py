e0f6dbf8047ac7de53704dd18c5639d1aeb2a03a
Abylay Kuvatov <abukuvatov@icloud.com>
2023-06-15 14:17:38 +0600

    #79 Переопределил модель Объявление

--- source/smarttender/models/trd_buy.py
+++ source/smarttender/models/trd_buy.py
@@ -1,24 +1,76 @@
 from django.db import models
+from django.utils import timezone
 
 
 class TrdBuy(models.Model):
-    ref_trade_methods = models.ForeignKey(
-        to="smarttender.RefTradeMethod",
-        on_delete=models.CASCADE
+    number_anno = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Номер объявления'
     )
-
-    numberAnno = models.CharField(max_length=255, null=True, blank=True)
-    nameKz = models.CharField(max_length=255, null=True, blank=True)
-    nameRu = models.CharField(max_length=255, null=True, blank=True)
-    totalSum = models.CharField(max_length=100, null=True, blank=True)
-    countLots = models.IntegerField(null=True, blank=True)
-    customerNameKz = models.TextField(null=True, blank=True)
-    customerNameRu = models.TextField(null=True, blank=True)
-    orgBin = models.CharField(max_length=255, null=True, blank=True)
-    orgPid = models.CharField(max_length=255, null=True, blank=True)
-    orgNameKz = models.CharField(max_length=255, null=True, blank=True)
-    orgNameRu = models.CharField(max_length=255, null=True, blank=True)
-    startDate = models.DateField(null=True, blank=True)
+    name_kz = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование на государственном языке'
+    )
+    name_ru = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование на русском языке'
+    )
+    total_sum = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Общая сумма запланированная для закупки (Сумма закупки)'
+    )
+    count_lots = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Количество лотов в объявлении'
+    )
+    customer_name_kz = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Наименование заказчика на государственном языке'
+    )
+    customer_name_ru = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Наименование заказчика на русском языке'
+    )
+    org_bin = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='БИН Организатора'
+    )
+    org_pid = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='ИД Организатора'
+    )
+    org_name_kz = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование организатора на государственном языке'
+    )
+    org_name_ru = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Наименование организатора на русском языке'
+    )
+    start_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата начала приема заявок'
+    )
     publish_date = models.CharField(
         max_length=100,
         null=True,
@@ -31,9 +83,20 @@ class TrdBuy(models.Model):
         blank=True,
         verbose_name="Крайний срок"
     )
-    itogiDatePublic = models.DateField(null=True, blank=True)
-    finYear = models.IntegerField(null=True, blank=True)
-
+    itogi_date_public = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата публикации итогов'
+    )
+    fin_year = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Финансовый год'
+    )
+    ref_trade_methods = models.ForeignKey(
+        to="smarttender.RefTradeMethod",
+        on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата и время создания"
@@ -42,3 +105,21 @@ class TrdBuy(models.Model):
         auto_now=True,
         verbose_name="Дата и время изменения"
     )
+    is_deleted = models.BooleanField(
+        null=False,
+        default=False,
+        verbose_name='Удалён'
+    )
+    deleted_at = models.DateTimeField(
+        null=True,
+        default=None,
+        verbose_name='Дата и время удаления'
+    )
+
+    def __str__(self):
+        return f'{self.number_anno} | {self.name_ru}'
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
