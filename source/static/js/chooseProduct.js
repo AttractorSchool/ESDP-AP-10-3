f8e79a5a45151e6df5765f668cf856927c7d7499
YerbolZhulniyazov <zhulniyazov@gmail.com>
2023-06-28 15:57:04 +0600

    #92 Обновил js-скрипт
    Добавил в скрипт чтобы после закрытия окна выбора товара был отправлен запрос на получение обновлененных данных

--- source/static/js/chooseProduct.js
+++ source/static/js/chooseProduct.js
@@ -1,7 +1,29 @@
-      $(document).ready(function() {
-        $('.similar-products-btn').click(function() {
-            let tenderId = $('#modal-tender_id').text();
-            let url = '/similar_products/' + tenderId + '/';
-            window.open(url, 'popup', 'width=1000px,height=600px');
-        });
+$(document).ready(function() {
+    $('.similar-products-btn').click(function() {
+        let tenderId = $('#modal-tender_id').text();
+        let url = '/similar_products/' + tenderId + '/';
+        let popupWindow = window.open(url, 'popup', 'width=1000px,height=600px');
+
+        popupWindow.onunload = function() {
+            $.ajax({
+                url: '/get_cell_data/',
+                type: 'GET',
+                data: {
+                    'cell_id': tenderId
+                },
+                success: function(data) {
+                    $('#modal-tender_id').text(data.tender_id);
+                    $('#modal-lot_number').text(data.lot_number);
+                    $('#modal-products').text(data.products);
+                    $('#modal-suppliers').text(data.suppliers);
+
+                    $('#myModal').modal('show');
+                },
+                error: function(xhr, status, error) {
+                }
+            });
+        };
     });
+});
+
+
