031a2f3fe5e790a879d689b3258cf29cc24f69ef
Alima Utkelbayeva <alima150201@gmail.com>
2023-06-28 15:56:12 +0600

    #74 Добавлены url для загрузки  пдф

--- source/smarttender/urls.py
+++ source/smarttender/urls.py
@@ -4,6 +4,7 @@ from smarttender.views import ProductListView, update_tender, get_cell_data
 from smarttender.views.enstru_codes import EnsTruListView, enstru_create_view
 from smarttender.views.mnn_products import find_similar_products, similar_products, selected_product
 from smarttender.views.tenders import TenderListView, tender_graphql_view, tender_save_view
+from smarttender.views.price_pdf import pdf_view,opt_pdf_price
 
 urlpatterns = [
     path('', TenderListView.as_view(), name='index'),
@@ -16,5 +17,8 @@ urlpatterns = [
     path('similar_products/<int:tender_id>/', similar_products, name='similar_products'),
     path('selected_product/', selected_product, name='selected_product'),
     path('get_cell_data/', get_cell_data, name='get_cell_data'),
-    path('enstru_create', enstru_create_view, name='enstru_create_view')
+    path('enstru_create', enstru_create_view, name='enstru_create_view'),
+    path('pdf_upload', pdf_view, name='pdf_view'),
+    path('opt_pdf_upload', opt_pdf_price, name='opt_pdf_view'),
+
 ]
